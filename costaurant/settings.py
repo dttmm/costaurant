142ba5a12283743e0de005550e087773c207c191
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-02 17:59:58 +0900

    menus앱 index, detail 페이지에서 버튼누르면 서로 이동할 수 있게 함

--- costaurant/settings.py
+++ costaurant/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2w0+m7m7!iw4=_tet3v!9%tm#5j+o5b$^+wrbj5peea2jyennf'
+SECRET_KEY = '8my4rcylk_xo5^*39&#^0zrdsl!37)hu6c0p1%%bu)q#6%0r^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'menus'
 ]
 
 MIDDLEWARE = [
@@ -105,13 +106,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- menus/views.py
+++ menus/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from datetime import datetime
+
 
 # Create your views here.
+def index(request):
+    today = datetime.now().date()
+    context = {'today': today}
+    return render(request, 'menus/index.html', context)
+
+
+def detail(request, menu=''):
+    return render(request, 'menus/detail.html')
