58c005eddbdd665065b09cdf3113b9150d2d0f7d
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-03 22:03:44 +0900

    빌드 준비 완료

--- costaurant/settings.py
+++ costaurant/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8my4rcylk_xo5^*39&#^0zrdsl!37)hu6c0p1%%bu)q#6%0r^z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
